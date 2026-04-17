095007d4204b05cf44d35992dcc81c9f34e419d0
FizzyElt <fizzyelt8786@gmail.com>
2026-04-17 19:52:12 +0800

    add bytes_util

---
+++ src/util/bytes_util.mli
@@ -0,0 +1,11 @@
+val map2_bytes : (int -> int -> int) -> bytes -> bytes -> bytes
+
+val xor_bytes : bytes -> bytes -> bytes
+
+val and_bytes : bytes -> bytes -> bytes
+
+val or_bytes : bytes -> bytes -> bytes
+
+val not_bytes : bytes -> bytes
+
+val equal_bytes_ct : bytes -> bytes -> bool
